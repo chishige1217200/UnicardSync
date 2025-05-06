a3ebe08fa4b4fb4bc681c30e00129b00a6c59e20
chishige1217200 <daichicchi01@gmail.com>
2025-05-07 00:36:07 +0900

    Add: 取込区分、ボタンの追加

--- TableForm.Designer.cs
+++ TableForm.Designer.cs
@@ -28,39 +28,80 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.table = new System.Windows.Forms.DataGridView();
-            ((System.ComponentModel.ISupportInitialize)(this.table)).BeginInit();
+            this.Table = new System.Windows.Forms.DataGridView();
+            this.ExportButton = new System.Windows.Forms.Button();
+            this.ImportButton = new System.Windows.Forms.Button();
+            this.TorikomiTypeComboBox = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.Table)).BeginInit();
             this.SuspendLayout();
             // 
-            // table
+            // Table
             // 
-            this.table.AllowUserToAddRows = false;
-            this.table.AllowUserToDeleteRows = false;
-            this.table.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.table.Location = new System.Drawing.Point(0, 30);
-            this.table.Name = "table";
-            this.table.ReadOnly = true;
-            this.table.RowTemplate.Height = 21;
-            this.table.Size = new System.Drawing.Size(1264, 651);
-            this.table.TabIndex = 0;
+            this.Table.AllowUserToAddRows = false;
+            this.Table.AllowUserToDeleteRows = false;
+            this.Table.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.Table.Location = new System.Drawing.Point(0, 30);
+            this.Table.Name = "Table";
+            this.Table.ReadOnly = true;
+            this.Table.RowTemplate.Height = 21;
+            this.Table.Size = new System.Drawing.Size(1264, 651);
+            this.Table.TabIndex = 0;
+            // 
+            // ExportButton
+            // 
+            this.ExportButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.ExportButton.Location = new System.Drawing.Point(1152, 3);
+            this.ExportButton.Name = "ExportButton";
+            this.ExportButton.Size = new System.Drawing.Size(100, 23);
+            this.ExportButton.TabIndex = 1;
+            this.ExportButton.Text = "データ出力";
+            this.ExportButton.UseVisualStyleBackColor = true;
+            this.ExportButton.Click += new System.EventHandler(this.ExportButton_Click);
+            // 
+            // ImportButton
+            // 
+            this.ImportButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.ImportButton.Location = new System.Drawing.Point(1046, 3);
+            this.ImportButton.Name = "ImportButton";
+            this.ImportButton.Size = new System.Drawing.Size(100, 23);
+            this.ImportButton.TabIndex = 2;
+            this.ImportButton.Text = "データ取込";
+            this.ImportButton.UseVisualStyleBackColor = true;
+            this.ImportButton.Click += new System.EventHandler(this.ImportButton_Click);
+            // 
+            // TorikomiTypeComboBox
+            // 
+            this.TorikomiTypeComboBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.TorikomiTypeComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.TorikomiTypeComboBox.FormattingEnabled = true;
+            this.TorikomiTypeComboBox.Location = new System.Drawing.Point(890, 5);
+            this.TorikomiTypeComboBox.Name = "TorikomiTypeComboBox";
+            this.TorikomiTypeComboBox.Size = new System.Drawing.Size(150, 20);
+            this.TorikomiTypeComboBox.TabIndex = 3;
             // 
             // TableForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1264, 681);
-            this.Controls.Add(this.table);
+            this.Controls.Add(this.TorikomiTypeComboBox);
+            this.Controls.Add(this.ImportButton);
+            this.Controls.Add(this.ExportButton);
+            this.Controls.Add(this.Table);
             this.Name = "TableForm";
-            this.Text = "明細一覧";
+            this.Text = "UnicardSync";
             this.Resize += new System.EventHandler(this.TableForm_Resize);
-            ((System.ComponentModel.ISupportInitialize)(this.table)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Table)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.DataGridView table;
+        private System.Windows.Forms.DataGridView Table;
+        private System.Windows.Forms.Button ExportButton;
+        private System.Windows.Forms.Button ImportButton;
+        private System.Windows.Forms.ComboBox TorikomiTypeComboBox;
     }
 }
 
--- TableForm.cs
+++ TableForm.cs
@@ -15,14 +15,33 @@ namespace UnicardSync
         public TableForm()
         {
             InitializeComponent();
+
+            TorikomiTypeComboBox.DataSource = TorikomiConfigHelper.Config;
+            TorikomiTypeComboBox.DisplayMember = "torikomiType";
+            TorikomiTypeComboBox.ValueMember = "torikomiType";
         }
 
         private void TableForm_Resize(object sender, EventArgs e)
         {
             Control control = (Control)sender;
 
-            this.table.Width = control.Width - 16;
-            this.table.Height = control.Height - 69;
+            this.Table.Width = control.Width - 16;
+            this.Table.Height = control.Height - 69;
+        }
+
+        private void ImportButton_Click(object sender, EventArgs e)
+        {
+            if (TorikomiTypeComboBox.SelectedValue == null || TorikomiTypeComboBox.SelectedValue.ToString() == "")
+            {
+                MessageBox.Show("データ取込を行う前に取込区分を選択してください。", "取込エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            //Console.WriteLine(torikomiTypeComboBox.SelectedValue.ToString());
+        }
+
+        private void ExportButton_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
