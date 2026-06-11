2b03e508d8be2d0bad908b4129ad97986d7e4165
chishige1217200 <daichicchi01@gmail.com>
2026-06-12 02:15:29 +0900

    Add: 取込画面の表レイアウトを2つに分割

---
+++ ConfirmCompare.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace UnicardSync
+{
+    partial class ConfirmCompare
+    {
+        /// <summary> 
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region コンポーネント デザイナーで生成されたコード
+
+        /// <summary> 
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を 
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Table = new System.Windows.Forms.DataGridView();
+            this.Table2 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.Table)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Table2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Table
+            // 
+            this.Table.AllowUserToAddRows = false;
+            this.Table.AllowUserToDeleteRows = false;
+            this.Table.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.DisplayedCellsExceptHeaders;
+            this.Table.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+            this.Table.Location = new System.Drawing.Point(0, 0);
+            this.Table.Name = "Table";
+            this.Table.ReadOnly = true;
+            this.Table.RowHeadersVisible = false;
+            this.Table.RowTemplate.Height = 21;
+            this.Table.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.Table.Size = new System.Drawing.Size(640, 651);
+            this.Table.TabIndex = 2;
+            // 
+            // Table2
+            // 
+            this.Table2.AllowUserToAddRows = false;
+            this.Table2.AllowUserToDeleteRows = false;
+            this.Table2.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.DisplayedCellsExceptHeaders;
+            this.Table2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+            this.Table2.Location = new System.Drawing.Point(640, 0);
+            this.Table2.Name = "Table2";
+            this.Table2.ReadOnly = true;
+            this.Table2.RowHeadersVisible = false;
+            this.Table2.RowTemplate.Height = 21;
+            this.Table2.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.Table2.Size = new System.Drawing.Size(640, 651);
+            this.Table2.TabIndex = 3;
+            // 
+            // ConfirmCompare
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.Table2);
+            this.Controls.Add(this.Table);
+            this.Name = "ConfirmCompare";
+            this.Size = new System.Drawing.Size(1280, 651);
+            this.Resize += new System.EventHandler(this.ConfirmCompare_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.Table)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Table2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.DataGridView Table;
+        public System.Windows.Forms.DataGridView Table2;
+    }
+}
---
+++ ConfirmCompare.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UnicardSync
+{
+    public partial class ConfirmCompare : UserControl
+    {
+        public ConfirmCompare()
+        {
+            InitializeComponent();
+
+            typeof(DataGridView).InvokeMember("DoubleBuffered", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.SetProperty, null, Table, new object[] { true });
+            typeof(DataGridView).InvokeMember("DoubleBuffered", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.SetProperty, null, Table2, new object[] { true });
+        }
+
+        private void ConfirmCompare_Resize(object sender, EventArgs e)
+        {
+            Control control = (Control)sender;
+
+            Table.Width = control.Width / 2;
+            Table2.Width = control.Width / 2;
+            Table2.Location = new Point(control.Width / 2, Table2.Location.Y);
+
+            Table.Height = control.Height;
+            Table2.Height = control.Height;
+        }
+    }
+}
